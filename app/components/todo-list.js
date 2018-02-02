c72f63a91275f220d8c0dbd15e3679ccea10e896
shibulijack <shibulijack@gmail.com>
2018-02-02 19:14:46 +0530

    Added filter

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -8,7 +8,9 @@ export default Ember.Component.extend({
 	allCompleted: Ember.computed('todos.@each.completed', function () {
 		return this.get('todos').isEvery('completed');
 	}),
-
+	remaining: Ember.computed.filterBy('todos', 'completed', false),
+	completed: Ember.computed.filterBy('todos', 'completed'),
+	selectedTodos: Ember.computed.reads('todos'),
 	actions: {
 		enableToggle() {
 			this.set('canToggle', true);
@@ -22,6 +24,25 @@ export default Ember.Component.extend({
 			let allCompleted = this.get('allCompleted');
 			this.get('todos').forEach(todo => Ember.set(todo, 'completed', !allCompleted));
 			this.get('dataLayer').persist();
+		},
+
+		clearCompleted() {
+			this.get('todos').removeObjects(this.get('completed'));
+			this.get('dataLayer').persist();
+        },
+		
+		filterTodo(filterBy) {
+            switch(filterBy) {
+                case 'all':
+                    this.set('selectedTodos', this.get('todos'));
+                    break;
+                case 'active':
+                    this.set('selectedTodos', this.get('remaining'));
+                    break;
+                case 'completed':
+                    this.set('selectedTodos', this.get('completed'));
+                    break;
+			}
         }
     }
 });
